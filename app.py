45619d7951d8cb300eb05b31ae51cd3ea6aaaf22
Senthil <rsenthilkumar.in@gmail.com>
2021-08-03 14:20:02 +0530

    format changes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import socket
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    hostname=socket.gethostname()
-    message="Hello World. Pod Name  : " + hostname
+    hostname = socket.gethostname()
+    message = "Hello World. Pod Name  : " + hostname
     return message
