9b2bbb22bec6c0c341582da2cbb43fb226c8234d
Senthil <rsenthilkumar.in@gmail.com>
2021-08-04 13:14:16 +0530

    format changes

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 import socket
+import redis
 
 app = Flask(__name__)
 
+redis = redis.Redis(
+    host='my-release-redis',
+    port='6379')
 
 @app.route('/')
 def hello_world():
     hostname = socket.gethostname()
     message = "Welcome to E-Commerce Application. Pod Name  : " + hostname
+    redis.set('pod',hostname)
     return message
