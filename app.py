b7c867ea28d51b2feb0fc89663d0b7dc16bc0775
Senthil <rsenthilkumar.in@gmail.com>
2021-08-04 16:04:32 +0530

    format changes

--- app.py
+++ app.py
@@ -4,14 +4,25 @@ import redis
 
 app = Flask(__name__)
 
-redis = redis.Redis(
+cache = redis.Redis(
     host='my-release-redis',
     port='6379',
     password='SVTvj7w2iP')
 
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 @app.route('/')
 def hello_world():
     hostname = socket.gethostname()
-    message = "Welcome to E-Commerce Application. Pod Name  : " + hostname
-    redis.set('pod', hostname)
+    count = get_hit_count()
+    message = "Welcome to E-Commerce Application. Pod Name  : " + hostname + "You've visited me {} times.\n".format(count)
     return message
