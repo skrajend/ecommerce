6780399cad45b982c4dd7fbcfc35f0641906384e
Senthil <rsenthilkumar.in@gmail.com>
2021-08-05 15:54:35 +0530

    format changes

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask
 import socket
 import redis
 import time
+from redis.sentinel import Sentinel
 
 app = Flask(__name__)
 
-cache = redis.Redis(
-    host='10.2.29.198',
+"""cache = redis.Redis(
+    host='ecom-backend-redis.default.svc.cluster.local',
     port='6379',
-    password='SVTvj7w2iP')
+    password='SVTvj7w2iP')"""
+
+redis_sentinel = Sentinel([('ecom-backend-redis.default.svc.cluster.local', '26379', 'SVTvj7w2iP')], socket_timeout=0.1)
+redis_master = redis_sentinel.master_for('mymaster', socket_timeout=0.1)
 
 
 def get_hit_count():
     retries = 5
     while True:
         try:
-            return cache.incr('hits')
+            return redis_master.incr('hits')
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
                 raise exc
