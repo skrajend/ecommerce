7a8bd45db315acb2b579d54390e710cf07e10787
Senthil <rsenthilkumar.in@gmail.com>
2021-08-05 16:08:27 +0530

    format changes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
     password='SVTvj7w2iP')"""
 
 redis_sentinel = Sentinel([('ecom-backend-redis.default.svc.cluster.local', '26379')], password='SVTvj7w2iP',
-                          socket_timeout=0.1)
+                          sentinel_kwargs={"password": "SVTvj7w2iP"}, socket_timeout=0.1)
 redis_master = redis_sentinel.master_for('mymaster', socket_timeout=0.1)
 
 
