4f7d979d45b2fa0ad6c2c1ec4ebdaa9433ccfd6a
Senthil <rsenthilkumar.in@gmail.com>
2021-08-05 18:24:23 +0530

    format changes

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 """cache = redis.Redis(
     host='ecom-backend-redis.default.svc.cluster.local',
     port='6379',
-    password='SVTvj7w2iP')"""
+    password='Kk5ue0Fx8B')"""
 
-redis_sentinel = Sentinel([('ecom-backend-redis.default.svc.cluster.local', '26379')], password='SVTvj7w2iP',
-                          sentinel_kwargs={"password": "SVTvj7w2iP"})
+redis_sentinel = Sentinel([('ecom-backend-redis.default.svc.cluster.local', '26379')], password='Kk5ue0Fx8B')
 redis_master = redis_sentinel.master_for('mymaster')
 
 
